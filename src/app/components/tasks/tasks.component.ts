61ea1bfb18f78a2675ba0f6af5109ae914cd4b9a
unknown <die6l0795@gmail.com>
2023-02-15 21:39:59 -0300

    App Terminada

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -18,5 +18,22 @@ export class TasksComponent implements OnInit {
     this.tasksService.getTasks().subscribe((tasks)=>
     this.tasks = tasks);   
   }
-   
+
+  deleteTask(task:Task){
+    this.tasksService.deleteTask(task)
+      .subscribe(()=>(
+        this.tasks = this.tasks.filter( t => t.id !== task.id )
+    ));
+  }
+
+  toggleReminder(task: Task){
+    task.reminder = !task.reminder
+    this.tasksService.updateTaskReminder(task).subscribe();
+  }   
+
+  addTask(task:Task){
+    this.tasksService.addTask(task).subscribe((task)=>(
+      this.tasks.push(task)
+    ));
+  }
 }
